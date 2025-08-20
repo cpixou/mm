d753fc4029a02048227efc473d18a71e7c93e97f
cpixou <215260096+cpixou@users.noreply.github.com>
2025-08-20 16:00:00 +0200

    update

--- mm.c
+++ mm.c
@@ -2,50 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void _(const char *l, mpz_t v)
+void print(const char *na, mpz_t vl)
 {
-  gmp_printf("  %s\n", l);
-  gmp_printf("    dec %Zd\n", v);
-  gmp_printf("    hex 0x%Zx\n", v);
-  gmp_printf("    oct 0%Zo\n", v);
+	gmp_printf("  %s\n", na);
+	gmp_printf("    dec %Zd\n", vl);
+	gmp_printf("    hex 0x%Zx\n", vl);
+	gmp_printf("    oct 0%Zo\n", vl);
 }
 
 int main(int ac, char **av, char **ev)
 {
-  (void)ev;
-  if(ac != 2)
-  {
-    fprintf(stderr, "usage: %s <bytes>\n", av[0]);
-    return 1;
-  }
+	(void) ev;
+	if (ac != 2)
+	{
+		fprintf(stderr, "usage: %s <bytes>\n", av[0]);
+		return 1;
+	}
 
-  int by = atoi(av[1]);
-  if(by <= 0)
-    return 1;
-  int bi = by * 8;
+	int by = atoi(av[1]);
+	if (by <= 0)
+		return 1;
+	int bi = by * 8;
 
-  mpz_t um, un, sm, sn;
-  mpz_inits(um, un, sm, sn, NULL);
+	mpz_t um, un, sm, sn;
+	mpz_inits(um, un, sm, sn, NULL);
 
-  mpz_set_ui(un, 0);
-  
-  mpz_ui_pow_ui(um, 2, bi);
-  mpz_sub_ui(um, um, 1);
+	mpz_set_ui(un, 0);
 
-  mpz_ui_pow_ui(sm, 2, bi - 1);
-  mpz_sub_ui(sm, sm, 1);
+	mpz_ui_pow_ui(um, 2, bi);
+	mpz_sub_ui(um, um, 1);
 
-  mpz_ui_pow_ui(sn, 2, bi - 1);
-  mpz_neg(sn, sn);
+	mpz_ui_pow_ui(sm, 2, bi - 1);
+	mpz_sub_ui(sm, sm, 1);
 
-  printf("[%d bytes => %d bits]\nunsigned\n", by, bi);
-  _("min", un);
-  _("max", um);
+	mpz_ui_pow_ui(sn, 2, bi - 1);
+	mpz_neg(sn, sn);
 
-  printf("signed\n");
-  _("min", sn);
-  _("max", sm);
+	printf("[%d bytes => %d bits]\nunsigned\n", by, bi);
+	print("min", un);
+	print("max", um);
 
-  mpz_clears(um, un, sm, sn, NULL);
-  return 0;
+	printf("signed\n");
+	print("min", sn);
+	print("max", sm);
+
+	mpz_clears(um, un, sm, sn, NULL);
+	return 0;
 }
